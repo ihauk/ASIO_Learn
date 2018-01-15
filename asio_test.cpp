e065152ce98b69bb26f4fd6e67236172efd05879
SOGOU-INC\zhuhao211054 <zhuhao211054@sogou-inc.com>
2018-01-15 15:50:22 +0800

    init

---
+++ asio_test.cpp
@@ -0,0 +1,26 @@
+﻿#include "asio.hpp"
+#include <iostream>
+#include <chrono>	
+#include <thread>
+
+void timer_callback(std::system_error ec) {
+	std::cout << "5 seconds  pass, thread id " << std::this_thread::get_id() << std::endl;
+}
+
+int main() {
+
+	asio::io_service io_service;
+	asio::steady_timer timer1(io_service, std::chrono::seconds(5));
+	timer1.async_wait( &timer_callback);
+
+	asio::steady_timer timer2(io_service, std::chrono::seconds(4));
+	timer2.async_wait(&timer_callback);
+
+	
+	std::cout << "before timer , thread id  " << std::this_thread::get_id() << std::endl;
+	int ret = io_service.run();
+
+	std::cout << "test " << ret << std::endl;
+
+	getchar();
+}	
